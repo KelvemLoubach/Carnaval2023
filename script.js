7d2190cdfb7f2ca9e26ce96426d0e459d448025e
Kelvem Loubach <kelvemloubach@outlook.com>
2023-02-16 12:36:01 -0300

    Versão 2.0

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 let larguraJanela = 0;
 let alturaJanela = 0;
 
-let ajusteTamanho = ()=>{
+let ajusteTamanho = () => {
     larguraJanela = window.innerWidth;
     alturaJanela = window.innerHeight;
 }
 
 ajusteTamanho();
 
-let clickShowButton = ()=>{
+let clickShowButton = () => {
 
-    if(document.getElementById('no')){
+    if (document.getElementById('no')) {
         document.getElementById('no').remove();
     }
-   
-let posicaox = Math.floor(Math.random()*alturaJanela)-160;
-let posicaoy = Math.floor(Math.random()*larguraJanela)-160;
 
-posicaox = posicaox < 0 ? 0 : posicaox;
-posicaoy = posicaoy < 0 ? 0 : posicaoy;
+    let posicaox = Math.floor(Math.random() * alturaJanela) - 160;
+    let posicaoy = Math.floor(Math.random() * larguraJanela) - 160;
 
-console.log(posicaox,posicaoy);
+    posicaox = posicaox < 0 ? 0 : posicaox;
+    posicaoy = posicaoy < 0 ? 0 : posicaoy;
 
-let buttonRandomic = document.createElement('button');
+    console.log(posicaox, posicaoy);
 
-buttonRandomic.className = 'button';
-buttonRandomic.innerHTML = '<i class="fa-solid fa-x"></i>';
-buttonRandomic.style.left = posicaoy + 'px';
-buttonRandomic.style.top = posicaox + 'px';
-buttonRandomic.style.position = 'absolute';
-buttonRandomic.setAttribute("id", "no");
-buttonRandomic.onclick = clickShowButton;
+    let buttonRandomic = document.createElement('button');
 
-document.body.appendChild(buttonRandomic);
+    buttonRandomic.className = 'button';
+    buttonRandomic.innerHTML = '<i class="fa-solid fa-x"></i>';
+    buttonRandomic.style.left = posicaoy + 'px';
+    buttonRandomic.style.top = posicaox + 'px';
+    buttonRandomic.style.position = 'absolute';
+    buttonRandomic.setAttribute("id", "no");
+    buttonRandomic.onclick = clickShowButton;
+
+    document.body.appendChild(buttonRandomic);
 
 };
 
