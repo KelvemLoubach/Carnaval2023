3ac2f4ff2e4f12642419ce0f07955e351d4afa38
Kelvem Loubach <kelvemloubach@outlook.com>
2023-02-19 11:13:00 -0300

    Versão final!

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ let clickShowButton = () => {
     let buttonRandomic = document.createElement('button');
 
     buttonRandomic.className = 'button';
-    buttonRandomic.innerHTML = '<i class="fa-solid fa-x fa-flip"></i>';
+    buttonRandomic.innerHTML = '<i class="fa-solid fa-x" style="--fa-beat-scale: 2.0;"></i>';
     buttonRandomic.style.left = posicaoy + 'px';
     buttonRandomic.style.top = posicaox + 'px';
     buttonRandomic.style.position = 'absolute';
@@ -34,5 +34,65 @@ let clickShowButton = () => {
 };
 
 
+let control = 5;
+
+let clickShowButtonYes = () => {
+
+    if(control > 0){
+
+    if (document.getElementById('yes')){
+        document.getElementById('yes').remove()
+    };
+
+    let posicaox = Math.floor(Math.random() * alturaJanela) - 160;
+    let posicaoy = Math.floor(Math.random() * larguraJanela) - 160;
+
+    posicaox = posicaox < 0 ? 0 : posicaox;
+    posicaoy = posicaoy < 0 ? 0 : posicaoy;
+
+    let buttonRandomic = document.createElement('button');
+
+    buttonRandomic.className = 'button';
+    buttonRandomic.innerHTML = `<i class="fa-solid fa-beat" style="--fa-beat-scale: 2.0;">${control}`;
+    buttonRandomic.style.left = posicaoy + 'px';
+    buttonRandomic.style.top = posicaox + 'px';
+    buttonRandomic.style.position = 'absolute';
+    buttonRandomic.setAttribute("id", "yes");
+    buttonRandomic.onclick = clickShowButtonYes;
+
+    document.body.appendChild(buttonRandomic);
+
+    control--;
+
+    console.log(control)
+}else{
+
+    
+    if (document.getElementById('yes')){
+        document.getElementById('yes').remove()
+    };
+
+    let posicaox = Math.floor(Math.random() * alturaJanela) - 160;
+    let posicaoy = Math.floor(Math.random() * larguraJanela) - 160;
+
+    posicaox = posicaox < 0 ? 0 : posicaox;
+    posicaoy = posicaoy < 0 ? 0 : posicaoy;
+
+    let buttonRandomic = document.createElement('button');
+
+    buttonRandomic.className = 'button';
+    buttonRandomic.innerHTML = '<i class="fa-solid fa-beat" style="--fa-beat-scale: 2.0;">'+' '+'Agora vai!';
+    buttonRandomic.style.left = posicaoy + 'px';
+    buttonRandomic.style.top = posicaox + 'px';
+    buttonRandomic.style.position = 'absolute';
+    buttonRandomic.setAttribute("id", "yes");
+    buttonRandomic.onclick = function() {
+        window.location.href = "contact.html";  };
+    
+    document.body.appendChild(buttonRandomic);
+
+    console.log(buttonRandomic)
+}
 
 
+};
